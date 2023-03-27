4810d2bde83cbf4b3bc18643f1c73e06d2fe51ca
dchooyc <dchooyc@gmail.com>
2023-03-27 12:25:37 +0800

    Add code in C for p1 part 1

--- p1/result/result.c
+++ p1/result/result.c
@@ -4,24 +4,42 @@
 
 #define MAX_LINES 3000
 #define MAX_LINE_LENGTH 256
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 char **conv_file(const char *target, int *num_lines);
-void prt_str_arr(char **lines, int num_lines);
+// to print a string array
+// void prt_str_arr(char **lines, int num_lines);
+void free_str_arr(char **lines, int num_lines);
+int max_cal_elf(char **snacks, int num_snacks);
 
 int main() {
     int num_lines = 0;
-    char **lines = conv_file("input.txt", &num_lines);
-
-    if (lines != NULL) {
-        prt_str_arr(lines, num_lines);
-
-        for (int i = 0; i < num_lines; i++) {
-            free(lines[i]);
-        }
-        free(lines);
-    }
+    char **lines = conv_file("../input.txt", &num_lines);
+    int max_elf = 0;
+    if (lines == NULL) { return 0; };
+    max_elf = max_cal_elf(lines, num_lines);
+    printf("The max cals on an elf is: %d\n", max_elf);
+    free_str_arr(lines, num_lines);
 
     return 0;
+}
+
+int max_cal_elf(char **snacks, int num_snacks) {
+    int maxElf = 0, curElf = 0;
+
+    for (int i = 0; i < num_snacks; i++) {
+        char *snack = snacks[i];
+        int cals = atoi(snack);
+
+        if (cals == 0) {
+            maxElf = MAX(maxElf, curElf);
+            curElf = 0;
+        } else {
+            curElf += cals;
+        }
+    }
+
+    return maxElf;
 }
 
 char **conv_file(const char *target, int *num_lines) {
@@ -57,8 +75,15 @@ char **conv_file(const char *target, int *num_lines) {
     return lines;
 }
 
-void prt_str_arr(char **lines, int num_lines) {
+// void prt_str_arr(char **lines, int num_lines) {
+//     for (int i = 0; i < num_lines; i++) {
+//         printf("%s", lines[i]);
+//     }
+// }
+
+void free_str_arr(char **lines, int num_lines) {
     for (int i = 0; i < num_lines; i++) {
-        printf("%s", lines[i]);
+        free(lines[i]);
     }
+    free(lines);
 }
