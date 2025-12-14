c53c99eec5256fe943ca7e079b2c8225a09ef7d0
Daniel Choo <dchooyc@gmail.com>
2025-12-15 03:51:58 +0800

    Add 2025 p11 part2

--- 2025/p11/main.go
+++ 2025/p11/main.go
@@ -9,19 +9,45 @@ import (
 func main() {
 	input := utils.ConvFile("input.txt")
 	children := createChildren(input)
-	part1 := 0
-	countPaths("you", "out", &part1, children)
+	part1 := countPaths("you", "out", children)
+	part2 := svrPaths(children)
 	fmt.Println(part1)
+	fmt.Println(part2)
 }
 
-func countPaths(cur, dst string, res *int, children map[string][]string) {
+func svrPaths(children map[string][]string) int64 {
+	sf := countPaths("svr", "fft", children)
+	sd := countPaths("svr", "dac", children)
+	fd := countPaths("fft", "dac", children)
+	df := countPaths("dac", "fft", children)
+	fo := countPaths("fft", "out", children)
+	do := countPaths("dac", "out", children)
+
+	return (sf * fd * do) + (sd * df * fo)
+}
+
+func countPaths(cur, dst string, children map[string][]string) int64 {
+	memo := make(map[string]int64)
+	return dfs(cur, dst, children, memo)
+}
+
+func dfs(cur, dst string, children map[string][]string, memo map[string]int64) int64 {
 	if cur == dst {
-		*res++
-		return
+		return 1
 	}
+
+	if val, ok := memo[cur]; ok {
+		return val
+	}
+
+	var res int64
+
 	for _, child := range children[cur] {
-		countPaths(child, dst, res, children)
+		res += dfs(child, dst, children, memo)
 	}
+
+	memo[cur] = res
+	return res
 }
 
 func createChildren(input []string) map[string][]string {
